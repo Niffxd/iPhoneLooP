6b0691d9332295f8fa4d229ae9ec28c74f0c1f0a
Niffxd <nicosanchez673@gmail.com>
2024-02-13 19:09:04 -0300

    changing silder animaiton timing state

---
+++ src/components/FunnelSlider/index.jsx
@@ -0,0 +1,16 @@
+import { list } from './images.js';
+import style from './FunnelSlider.module.css';
+
+export default function FunnelDevice () {
+  return (
+    <div className={style.funnel_slider_container}>
+      {
+        list.map(({ id, imgPath }) => {
+          return (
+            <img key={id} src={imgPath} alt='funnel-slider-image' />
+          );
+        })
+      }
+    </div>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import HomeSlider from '../../components/HomeSlider'; // eslint-disable-line
+import FunnelDevice from '../../components/FunnelDevice'; // eslint-disable-line
 
 export default function Home () {
   return (
     <>
       <HomeSlider />
+      <FunnelDevice />
     </>
   );
 }
