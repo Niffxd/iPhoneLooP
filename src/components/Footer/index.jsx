c45ea3ad61584ed5b7fbe0a1c84cc8ae04874ef2
Niffxd <nicosanchez673@gmail.com>
2024-03-10 02:36:57 -0300

    section 4 added to testing and some images and stylesheets

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'; //eslint-disable-line
+import { contactItems, foundSVG } from './contactItems';
+import style from './Footer.module.css';
+
+export default function Footer () {
+  return (
+    <footer className={style.footer_container}>
+      <div className={style.contact_container}>
+        {
+          contactItems.map(({ name, link, text }) => {
+            return (
+              <Link to={link} target='_blank'>
+                <article key={name} className={style.card}>
+                  <div className={style.icon}>
+                    <img src={foundSVG(name)} alt={name} />
+                  </div>
+                  <div className={style.information}>
+                    <h3>{name}</h3>
+                    <p>{text}</p>
+                  </div>
+                </article>
+              </Link>
+            );
+          })
+        }
+      </div>
+    </footer>
+  );
+}
