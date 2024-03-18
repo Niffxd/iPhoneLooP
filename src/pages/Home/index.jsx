75732f14ca5bf6f753f996c2ca82473e8f65bbb7
Niffxd <nicosanchez673@gmail.com>
2024-03-18 12:51:30 -0300

    adding review section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import HomeSlider from '../../components/HomeSlider'; // eslint-disable-line
 import FunnelDevice from '../../components/FunnelDevice'; // eslint-disable-line
 import WhatCanIRepair from '../../components/WhatCanIRepair'; // eslint-disable-line
+import Reviews from '../../components/Reviews'; // eslint-disable-line
 
 export default function Home () {
   return (
@@ -8,6 +9,7 @@ export default function Home () {
       <HomeSlider />
       <FunnelDevice />
       <WhatCanIRepair />
+      <Reviews />
     </>
   );
 }
