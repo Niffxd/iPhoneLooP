507d483346a97d0d47f78c4893ee43159219e34f
Niffxd <nicosanchez673@gmail.com>
2024-03-27 00:34:19 -0300

    changing ready for repair section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import HomeSlider from '../../components/HomeSlider'; // eslint-disable-line
 import FunnelDevice from '../../components/FunnelDevice'; // eslint-disable-line
 import WhatCanIRepair from '../../components/WhatCanIRepair'; // eslint-disable-line
 import Reviews from '../../components/Reviews'; // eslint-disable-line
+import ReadyForYourRepair from '../../components/ReadyForYourRepair'; // eslint-disable-line
 
 export default function Home () {
   return (
@@ -10,6 +11,7 @@ export default function Home () {
       <FunnelDevice />
       <WhatCanIRepair />
       <Reviews />
+      <ReadyForYourRepair />
     </>
   );
 }
