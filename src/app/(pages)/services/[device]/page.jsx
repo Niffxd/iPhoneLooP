64cc8fadc2356d91fec09cbf9d80fd2bb1989acb
Nico Sanchez <nicosanchez673@gmail.com>
2024-11-01 03:23:38 -0300

    updating redirections

--- src/app/(pages)/services/[device]/page.jsx
+++ src/app/(pages)/services/[device]/page.jsx
@@ -2,13 +2,14 @@
 
 import { useState } from 'react';
 import { devices } from '@/components/Devices/devices.js';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import style from './Device.module.css';
 
 export default function Device () {
   const { device } = useParams();
+  const router = useRouter();
   
-  const [currentDevice, setCurrentDevice] = useState(device.replaceAll('%20', ' ')); // eslint-disable-line
+  const [currentDeviceName, setCurrentDeviceName] = useState(device.replaceAll('%20', ' ')); // eslint-disable-line
   const [currentPrice, setCurrentPrice] = useState(0);
 
   const handlerSummation = (checked, price) => {
@@ -21,31 +22,35 @@ export default function Device () {
     console.log('handleSubmitForm')
   }
 
+  const currentDevice = devices.find(device => device.name === currentDeviceName)
+
   return (
     <div className={style.device_repair_container}>
-      <h1>iPhone {currentDevice} Repair</h1>
+      <h1>iPhone {currentDeviceName} Repair</h1>
       <br />
       <p>We are experts in repairing iPhon es to microchip level including the most advanced faults. <b>We use original recycled and refurbished screens</b>. These screens are either pulled or refurbished in-house to a like-new condition. , We have also high quality aftermarket screens. Please read our FAQ&apos;s below our pricing and please do not hesitate in submitting a question. Our average repair time is very fast  and it depends on the type of repair required.</p>
       <br />
       <form onSubmit={() => {}} className={style.form_budget}>
       <h4>Select your repairs:</h4>
         {
-          devices.find(device => device.name === currentDevice)
-            .prices.map((repair, index) => {
-              return (
-                <div className={style.repair_option_container} key={index}>
-                  <label htmlFor={`${repair.name}-name`}>
-                    <input type="checkbox" name={`${repair.name}-name`} id={`${repair.name}-id`} onChange={(e) => handlerSummation(e.target.checked, repair.price)} />
-                    {repair.name}
-                  </label>
-                  {
-                    typeof(repair.price) === 'number'
-                      ? <p>$ {repair.price}.00</p>
-                      : <p>{repair.price}</p>
-                  }
-                </div>
-              )
-            })
+          currentDevice === undefined
+            ? router.push("/services")
+            : currentDevice
+              .prices.map((repair, index) => {
+                return (
+                  <div className={style.repair_option_container} key={index}>
+                    <label htmlFor={`${repair.name}-name`}>
+                      <input type="checkbox" name={`${repair.name}-name`} id={`${repair.name}-id`} onChange={(e) => handlerSummation(e.target.checked, repair.price)} />
+                      {repair.name}
+                    </label>
+                    {
+                      typeof(repair.price) === 'number'
+                        ? <p>$ {repair.price}.00</p>
+                        : <p>{repair.price}</p>
+                    }
+                  </div>
+                )
+              })
         }
         <br />
         <hr />
