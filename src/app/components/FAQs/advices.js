2a5a6b1dea854f0deb3f070bcf5ffcbd8e1215ca
Nico Sanchez <nicosanchez673@gmail.com>
2024-11-09 02:40:40 -0300

    added remaining prices and some styles to prices

--- src/app/components/FAQs/advices.js
+++ src/app/components/FAQs/advices.js
@@ -1,34 +1,68 @@
+import { describe } from "node:test";
+
 export const faqs = [
   {
     title: "How long does an iPhone repair take?",
-    describe: "The average repair time for an iPhone is 2 hours. Should your iPhone have a logic board based issue, this will require to have a diagnostic completed first. The additional repair time will be advised once we have diagnosed the problem.",
+    describe: "The average repair time for an iPhone is 1-2 hours. Except Screen replacement usually takes 30min. Should your iPhone have a logic board based issue, this will require to have a diagnostic completed first. The additional repair time will be advised once we have diagnosed the problem.",
   },
   {
-    title: "Will my iPhone still show battery health in settings after a battery repair? ",
-    describe: "For iPhone 12 models and above running iOS 18 or later, we configure the newly installed battery within the settings to confirm its originality and battery health. For older iPhones, we use high-quality OEM batteries that perform at the same level, as original batteries are no longer available due to their age. All batteries undergo rigorous testing to ensure perfect functionality, and we only install the best-performing ones.",
+    title: "Do I have to pay the diagnostic fee?",
+    describe: "When diagnosing a phone and reporting the failure, if the customer decides to repair the phone, there is no charge for the diagnosis. If the customer decides not to repair the phone the diagnostic fee is $40.",
   },
   {
-    title: "Will my replacement screen have the Apple display repair notification?",
-    describe: "No, following our repair your iPhone will not show an Apple display notification. Through our repair we use an Original OLED display and transfer the screen's microchip from your old screen to new screen. This ensures perfect touch calibration and no display notification.",
+    title: "Can you repair a damaged logic board?",
+    describe: "Yes, we would complete a full diagnostic prior to quoting for our repair. If your logic board has been repaired before visiting us, we would ask you to provide as much detail about the previous repair(s) as possible.",
   },
   {
-    title: "Does my iPhone lose the True Tone functionality after repair?",
-    describe: "No, we ensure the True Tone functionality is transferred from your old screen to your new screen. As we use an Original OLED screen for your repair, the screen resolution, pixel density and colour density will be perfect (as you'd expect it to be after having it repaired at the Apple Store).",
+    title: "Will I be charged if you are unable to complete a repair?",
+    describe: "At Phone Loop, we work on a no-fix-no-fee policy. If we are unable to complete your repair, you will not be charged for parts or labour.",
   },
   {
-    title: "Do you offer a budget iPhone screen option?",
-    describe: "No, offering a budget screen would be against our company values. We operate iCorrect to bring more sustainable repairs to consumers using originally pulled parts. Budget screens break easily and can damage the logic board by pulling too much power. It's cheap for a reason!",
+    title: "How long does it take to diagnose an unknown fault?",
+    describe: "Our full diagnostic procedure take on average 2-3 hours to complete.",
   },
   {
-    title: "I've broken the back of my iPhone, can you repair just the glass?",
-    describe: "We do not offer just the rear glass repair on an iPhone. This is because the rear glass contains an Apple logo, the glass which is used in the industry is either counterfeit or does not contain an Apple logo. Glass which contains an Apple logo is an infringement of Apple's copyright. We offer a full housing repair which is the frame of the iPhone and the rear glass. It is a disassembled part from a new or used iPhone, this way we confirm it is an original part and not an infringement of Apple's copyright. If you were to have a back glass fitted without an Apple logo it would immediately devalue your iPhone when you come to sell it or trade it in. We would absolutely advise against having a no logo glass fitted. It's one of our red flags when it comes to choosing a repair company for your iPhone.",
+    title: "Do I need to back-up my data before my repair?",
+    describe: "Whilst we don't experience data loss through our repairs, we would recommend completing an iCloud back-up prior to your repair as good practice.",
+  },
+  {
+    title: "Do your prices include parts & labour?",
+    describe: "Yes, all of our prices include parts and labour.",
+  },
+  {
+    title: "Are your repairs covered by a warranty?",
+    describe: "Yes, we provide a 6 Months warranty for all of our repairs which includes faults with your screen but excludes accidental damage.",
+  },
+  {
+    title: "Do I need an appointment?",
+    describe: "Yes, for our Walk-in Service you will require an appointment. All three of our services can be booked online. Please select your repair(s) on this page and service on the next page. Also you can book by Whatsapp or messenger.",
+  },
+  {
+    title: "What is the difference between the Screen qualities?",
+    describe: "The short answer: Quality. Soft Oled Screens are very similar to Original Screen. LCD Screens, the quality is lower and the battery consumption may increase a little bit.",
+  },
+  {
+    title: "My rear camera lens is cracked, do I need a camera repair too?",
+    describe: "Provided the lens is only cracked and not completely shattered it is likely that you would just need a lens replacement. When there is a hole in the lens, dust particles and other debris can damage the sensor underneath and it is in these cases that you would need a rear camera replacement. You can check if your sensor is damaged by aiming your camera at a white, bright surface, if there are grey dots or marks on what should be a white image, then your sensor is likely damaged. If you are unsure, our customer advisor teams will be able to tell you when we receive the device.",
   },
   {
     title: "Do you need my passcode for a repair?",
     describe: "Yes, we ask for your passcode so we can fully test your device before and after the repair. As part of our process we will test all functions of your device using industry leading software.",
   },
   {
-    title: "My rear camera lens is cracked, do I need a camera repair too?",
-    describe: "Provided the lens is only cracked and not completely shattered it is likely that you would just need a lens replacement. When there is a hole in the lens, dust particles and other debris can damage the sensor underneath and it is in these cases that you would need a rear camera replacement. You can check if your sensor is damaged by aiming your camera at a white, bright surface, if there are grey dots or marks on what should be a white image, then your sensor is likely damaged. If you are unsure, our customer advisor teams will be able to tell you when we receive the device.",
+    title: "I've broken the back of my iPhone, can you repair just the glass?",
+    describe: "Yes. Usually Back glass replacement takes around 2-3 hours.",
+  },
+  {
+    title: "Does my iPhone lose the True Tone functionality after repair?",
+    describe: "No, we ensure the True Tone functionality is transferred from your old screen to your new screen.",
+  },
+  {
+    title: "Will my replacement screen have the Apple display repair notification?",
+    describe: "It depends on the quality chosen. When replacing the display with an incell or soft oled quality display, an unknown part message will appear in settings but does not affect usage. Original/refurbished screens for iPhone 12 models and above running iOS 18 or later when configuring them, the unknown part message no longer appears. As they are Remanufactured-Pulled screens, a used message appears.",
+  },
+  {
+    title: "Will my iPhone still show battery health in settings after a battery repair?",
+    describe: "For iPhone 12 models and above running iOS 18 or later, we configure the newly installed battery within the settings to confirm battery health. For older iPhones, we use high-quality OEM batteries that perform at the same level, as original batteries are no longer available due to their age. All batteries undergo rigorous testing to ensure perfect functionality, and we only install the best-performing ones.",
   },
 ]
