1b122fa270db6f3596fe74954bb688a58febb147
Nico Sanchez <nicosanchez673@gmail.com>
2024-11-17 03:56:41 -0300

    commit just for saving changes x2

--- src/app/components/Reviews/index.jsx
+++ src/app/components/Reviews/index.jsx
@@ -1,14 +1,25 @@
 'use client'
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
 import { fetchData } from './reviews';
 import CardReview from './CardReview';
 import CardOwner from './CardOwner.jsx';
-import Loading from '../Loader';
 import style from './Reviews.module.css';
 
 export default function Reviews() {
   const [data, setData] = useState([]);
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
 
   useEffect(() => {
     fetchData(setData, 0); // < 0 -> to test loading, 0 -> to receive from database, > 1 -> attempts to try to receive data from API
@@ -18,22 +29,28 @@ export default function Reviews() {
     <section className={style.reviews_container}>
       <h2>What people says about me</h2>
       <CardOwner />
-      {
-        !data?.length || !data
-          ? <Loading />
-          : data.map(
+      <div className={style.embla} ref={emblaRef}>
+        <div className={style.embla__container}>
+          {data?.map(
             ({
               authorAttribution,
               relativePublishTimeDescription,
               originalText
-            }) => <CardReview
+            }) =>
+              <div
                 key={authorAttribution.uri}
-                author={authorAttribution}
-                date={relativePublishTimeDescription}
-                text={originalText.text}
-              />
+                className={style.embla__slide}
+              >
+                <CardReview
+                  author={authorAttribution}
+                  date={relativePublishTimeDescription}
+                  text={originalText.text}
+                />
+              </div>
           )
-      }
+          }
+        </div>
+      </div>
     </section>
   );
 }
